88942ae02ab3fec5e7646205794e039fe2cdd86d
herber <hbatress@miumg.edu.gt>
2024-07-09 21:07:08 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/*
+import logo from './logo.svg';
+import './App.css';
+*/
+import'./style/animation.css';
+import './style/style.css'
+import  {Encabezado}  from './componet/encabezado';
+function App() {
+  return (
+    <div className="App">
+              <Encabezado/>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componet/encabezado.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Encabezado = () => {
+  return (
+   <header>
+    <ul className='Information'>
+    <a href='#inicio'>Inicio</a>
+    <a href='Sobremi'>Sobre Mi</a>
+    <a href='proyect'>Proyectos</a>
+    <a href='Contacto'>Contacto</a>
+    </ul>
+   </header>
+  )
+}
